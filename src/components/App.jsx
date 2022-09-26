0d984fbf7a2733aa69cc5a04b76e1a523819a237
Antonyly <antonlyzun@gmail.com>
2022-09-26 18:46:46 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { ContactForm } from './ContactForm/ContactForm'
 import { ContactList } from './ContactList/ContactList'
 import { Filter } from './Filter/Filter'
 import css from './ContactForm/ContactForm.module.css'
+  const CONTACTS_KEY = 'Contact'
 
 export class App extends Component {
 
@@ -13,6 +14,24 @@ export class App extends Component {
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },],
     filter: ''
+  }
+
+    componentDidMount() {
+    const savedContacts = localStorage.getItem(CONTACTS_KEY)
+
+    if (savedContacts?.length > 0) {
+      this.setState({
+      contacts: JSON.parse(savedContacts)
+    })
+    }
+  }
+  
+  componentDidUpdate(prevState) {
+    const { contacts } = this.state
+    
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem(CONTACTS_KEY, JSON.stringify(contacts))
+    }
   }
 
   isDuplicate = ({ name }) => {
